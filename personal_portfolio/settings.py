d627a008c7006af84a8a2aaad177f46eef181ef8
Subbu Sethuraman <snrmanian@gmail.com>
2021-05-08 12:18:11 +0530

    sqllite

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -78,13 +78,9 @@ WSGI_APPLICATION = 'personal_portfolio.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE':'django.db.backends.mysql',
-        'NAME':'pp',
-        'USER':"subbu",
-        'PASSWORD':"subbu",
-        'HOST':'127.0.0.1',
-        'PORT':'3306',       
+'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
